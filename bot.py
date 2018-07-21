ded4c974564095f54a51bcb25159aaded7119ad4
kdrag0n <dragon@khronodragon.com>
2018-07-20 22:25:02 -0700

    Add more stats

--- bot.py
+++ bot.py
@@ -398,8 +398,11 @@ Time: {el_str}'''
         return f'''Stats:
     \u2022 Messages received: {st['received']}
     \u2022 Messages sent: {st['sent']}
+    \u2022 Percent of total messages sent: {'%.2f' % ((float(st['sent']) / float(st['received'])) * 100)}%
     \u2022 Commands processed: {st['processed']}
-    \u2022 Snippets replaced: {st['replaced']}'''
+    \u2022 Snippets replaced: {st['replaced']}
+    \u2022 Percent of sent messages processed as commands: {'%.2f' % ((float(st['processed']) / float(st['sent'])) * 100)}%
+    \u2022 Percent of sent messages with snippets: {'%.2f' % ((float(st['replaced']) / float(st['sent'])) * 100)}%'''
 
     @command.desc('Get plain text of a message (debug)')
     def cmd_gtx(self, msg: tg.Message) -> str:
