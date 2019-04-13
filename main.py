8562bf665244acc418c992c88c8170c856a8d98b
Danny Lin <danny@kdrag0n.dev>
2019-04-13 16:05:05 -0700

    main: Remove double new lines
    This looks ugly. Don't follow PEP-8 in this regard.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- main.py
+++ main.py
@@ -3,14 +3,12 @@
 import bot
 import toml
 
-
 def main():
     config: bot.Config = toml.load('config.toml')
 
     inst: bot.Bot = bot.Bot()
     inst.setup('main', config)
     inst.start()
-
 
 if __name__ == '__main__':
     main()
