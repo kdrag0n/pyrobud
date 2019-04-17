39dac5c23e572026a48d214234fd06b110a22a79
Danny Lin <danny@kdrag0n.dev>
2019-04-16 22:54:23 -0700

    antibot: Expand message criteria
    A new form of scam message has been seen that include a phone number and
    "investment" in the text. Match more messages to improve chances of
    automatically catching future variants of these scams without manual
    intervention.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- modules/antibot.py
+++ modules/antibot.py
@@ -6,6 +6,15 @@ import time
 class AntibotModule(module.Module):
     name = 'Antibot'
 
+    suspicious_keywords = [
+        'investment',
+        'profit',
+        'binance',
+        'binanse',
+        'bitcoin',
+        'testnet'
+    ]
+
     def on_load(self):
         if 'antibot' not in self.bot.config:
             self.bot.config['antibot'] = {
@@ -18,14 +27,47 @@ class AntibotModule(module.Module):
             if 'group_ids' not in self.bot.config['antibot']:
                 self.bot.config['antibot']['group_ids'] = []
 
-    def contains_link(self, text):
-        if not text:
+    def msg_has_suspicious_entity(self, msg):
+        if not msg.entities:
             return False
 
-        return 'https://' in text or 'http://' in text or 't.me/' in text
+        for entity in msg.entities:
+            if entity.type == 'url' or entity.type == 'text_link':
+                return True
+            if entity.type == 'email':
+                return True
+            if entity.type == 'phone_number':
+                return True
 
-    def is_suspicious(self, msg):
-        return msg.forward_from or msg.forward_from_name or msg.forward_from_chat or msg.forward_from_message_id or self.contains_link(msg.text)
+        return False
+
+    def msg_is_forwarded(self, msg):
+        return msg.forward_from or msg.forward_from_name or msg.forward_from_chat or msg.forward_from_message_id
+
+    def msg_has_suspicious_keyword(self, msg):
+        if not msg.text:
+            return False
+
+        l_text = msg.text.lower()
+        for kw in self.__class__.suspicious_keywords:
+            if kw in l_text:
+                return True
+
+        return False
+
+    def msg_content_suspicious(self, msg):
+        return self.msg_is_forwarded(msg) or self.msg_has_suspicious_entity(msg) or self.msg_has_suspicious_keyword(msg)
+
+    def msg_is_suspicious(self, msg):
+        enabled_in_group = msg.chat and msg.chat.type == "supergroup" and msg.chat.id in self.bot.config['antibot']['group_ids']
+        user_is_not_us = msg.from_user and msg.from_user.id != self.bot.uid
+        has_date = msg.date
+
+        # Lazily evalulate suspicious content as it is significantly more expensive
+        if enabled_in_group and user_is_not_us and has_date:
+            return self.msg_content_suspicious(msg)
+
+        return False
 
     def take_action(self, msg):
         self.bot.client.send_message(msg.chat.id, f'/ban Spambot detected (ID: `{msg.from_user.id}`)', reply_to_message_id=msg.message_id)
@@ -35,7 +77,7 @@ class AntibotModule(module.Module):
         self.log_stat('spambots_banned')
 
     def on_message(self, msg):
-        if msg.chat and msg.chat.type == "supergroup" and msg.chat.id in self.bot.config['antibot']['group_ids'] and msg.from_user and msg.from_user.id != self.bot.uid and msg.date and self.is_suspicious(msg):
+        if self.msg_is_suspicious(msg):
             member = self.bot.client.get_chat_member(msg.chat.id, msg.from_user.id)
             if not member.date:
                 return
@@ -48,7 +90,7 @@ class AntibotModule(module.Module):
     @command.desc('Toggle the antibot auto-moderation feature in this group')
     def cmd_antibot(self, msg):
         if not msg.chat or msg.chat.type != "supergroup":
-            return "__This chat isn't eligible for antibot.__"
+            return "__Antibot can only be used in supergroups.__"
 
         state = msg.chat.id in self.bot.config['antibot']['group_ids']
         state = not state
