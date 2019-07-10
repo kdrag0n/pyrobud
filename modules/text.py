8a48fb8a103ec00f239c75e340b9c5a5f11d0515
Danny Lin <danny@kdrag0n.dev>
2019-07-09 19:21:23 -0700

    text: Add sarcasm text filter
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- modules/text.py
+++ modules/text.py
@@ -1,5 +1,6 @@
 import command
 import module
+import random
 
 class TextModule(module.Module):
     name = 'Text'
@@ -11,3 +12,18 @@ class TextModule(module.Module):
             return '__Hex codepoint required.__'
 
         return chr(int(codepoint, 16))
+
+    @command.desc('Apply a sarcasm/mocking filter to the given text')
+    @command.alias('sar', 'sarc', 'scm', 'mock')
+    async def cmd_sarcasm(self, msg, text):
+        if not text:
+            return '__Text required.__'
+
+        chars = list(text)
+        for idx, ch in enumerate(chars):
+            if random.choice((True, False)):
+                ch = ch.upper()
+
+            chars[idx] = ch
+
+        return ''.join(chars)
