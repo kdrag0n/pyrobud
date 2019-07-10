f5a6ed1f6e775d502ef8349246eb0c1b7f9ad910
Danny Lin <danny@kdrag0n.dev>
2019-07-09 23:51:47 -0700

    text: Add @admin mass-mentioning command
    Useful for reporting things in groups without bots capable of doing so.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- modules/text.py
+++ modules/text.py
@@ -1,3 +1,4 @@
+import telethon as tg
 import command
 import module
 import random
@@ -43,20 +44,25 @@ class TextModule(module.Module):
 
     @command.desc('Mention everyone in this group (**DO NOT ABUSE**)')
     @command.alias('evo', '@everyone')
-    async def cmd_everyone(self, msg, comment):
+    async def cmd_everyone(self, msg, comment, *, tag='everyone', filter=None):
         if not msg.is_group:
             return '__This command can only be used in groups.__'
 
-        mention_text = '@\U000e0020everyone'
+        mention_text = f'@\U000e0020{tag}'
         if comment:
             mention_text += ' ' + comment
 
         mention_slots = 4096 - len(mention_text)
 
         chat = await msg.get_chat()
-        async for user in self.bot.client.iter_participants(chat):
+        async for user in self.bot.client.iter_participants(chat, filter=filter):
             mention_text += f'[\u200b](tg://user?id={user.id})'
             mention_slots -= 1
 
         await msg.respond(mention_text, reply_to=msg.reply_to_msg_id)
         await msg.delete()
+
+    @command.desc('Mention all admins in a group (**DO NOT ABUSE**)')
+    @command.alias('adm', '@admin')
+    async def cmd_admin(self, msg, comment):
+        await self.cmd_everyone(msg, comment, tag='admin', filter=tg.tl.types.ChannelParticipantsAdmins)
