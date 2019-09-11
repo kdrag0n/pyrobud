e738f08fa8bfc4620793304ddf7ea87b2ef116cf
Danny Lin <danny@kdrag0n.dev>
2019-09-10 22:53:53 -0700

    antibot: Ban with "auto-detected" comment
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- modules/antibot.py
+++ modules/antibot.py
@@ -99,7 +99,7 @@ class AntibotModule(module.Module):
 
         # Log the event
         self.log.info(f'Banned spambot with ID {sender.id} in group "{chat.title}"')
-        await msg.reply(f"❯❯ **Banned spambot** with ID `{sender.id}`")
+        await msg.reply(f"❯❯ **Banned auto-detected spambot** with ID `{sender.id}`")
         self.bot.dispatch_event_nowait("stat_event", "spambots_banned")
 
         # Delete the spam message
