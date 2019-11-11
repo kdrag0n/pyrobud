c709d1133285cc531b50326738b8fb4f07f8aeb9
Danny Lin <danny@kdrag0n.dev>
2019-11-11 12:51:49 -0800

    time: Improve duration format aesthetics
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- util/time.py
+++ util/time.py
@@ -26,14 +26,14 @@ def format_duration_us(t_us):
         rem_h = t_h % 24
         rem_m = t_m % 60
         rem_s = t_s % (24 * 60 * 60) % 60
-        return "%dd%dh%dm%ds" % (t_d, rem_h, rem_m, rem_s)
+        return "%dd %dh %dm %ds" % (t_d, rem_h, rem_m, rem_s)
     elif t_h >= 1:
         rem_m = t_m % 60
         rem_s = t_s % (60 * 60) % 60
-        return "%dh%dm%ds" % (t_h, rem_m, rem_s)
+        return "%dh %dm %ds" % (t_h, rem_m, rem_s)
     elif t_m >= 1:
         rem_s = t_s % 60
-        return "%dm%ds" % (t_m, rem_s)
+        return "%dm %ds" % (t_m, rem_s)
     elif t_s >= 10:
         return "%ds" % t_s
     elif t_ms >= 10:
