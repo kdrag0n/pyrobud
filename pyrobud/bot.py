c6d5e7d0ac299643c2448d6106414e246a0f2b09
Danny Lin <danny@kdrag0n.dev>
2019-11-18 15:01:37 -0800

    bot: Read aliases from command info object
    This is more consistent and fixes aliases not working after commit
    fafee70.
    
    The bot core should never have been relying on an implementation detail
    of the command enumeration system anyway.
    
    Fixes: fafee70 ("command: Prefix decorator-added variables with "cmd_"")
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- pyrobud/bot.py
+++ pyrobud/bot.py
@@ -56,7 +56,7 @@ class Bot:
 
         self.commands[name] = info
 
-        for alias in getattr(func, "aliases", []):
+        for alias in info.aliases:
             if alias in self.commands:
                 orig = self.commands[alias]
                 raise module.ExistingCommandError(orig, info, alias=True)
