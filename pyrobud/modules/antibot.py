kdrag0n/pyrobud

commit 94a6b8671781f88ddd41d12fdc72b4626b8f6e48

antibot: Allow forwarding oneself's own messages
Many people will mass-join various support groups in a desperate attempt
to obtain help. In doing so, they forward their single question message
which contains an image/link as their first message in each group and
thus get their messages marked as suspicious.

Such behavior can be considered spammy, but that's out of the scope of
what this module is defending against.

Signed-off-by: Danny Lin <danny@kdrag0n.dev>

pyrobud/modules/antibot.py
@@ -66,7 +66,7 @@ class AntibotModule(module.Module):
     def msg_type_suspicious(self, msg):
         return msg.contact or msg.geo or msg.game
 
-    def msg_data_is_suspicious(self, msg):
+    async def msg_data_is_suspicious(self, msg):
         incoming = not msg.out
         has_date = msg.date
         forwarded = msg.forward
@@ -76,6 +76,15 @@ class AntibotModule(module.Module):
         if incoming and has_date:
             # Lazily evalulate suspicious content as it is more expensive
             if forwarded:
+                # Spambots don't forward their own messages; they mass-forward
+                # messages from central coordinated channels for maximum efficiency
+                # This protects users who forward questions with links/images to
+                # various support chats asking for help (arguably, that's spammy,
+                # but it's not what we're defending against here)
+                sender = await msg.get_sender()
+                if msg.forward.from_id == sender.id or msg.forward.from_name == tg.utils.get_display_name(sender):
+                    return False
+
                 # Screen forwarded messages more aggressively
                 return msg.photo or self.msg_type_suspicious(msg) or self.msg_content_suspicious(msg)
             else:
@@ -86,7 +95,7 @@ class AntibotModule(module.Module):
 
     async def msg_is_suspicious(self, msg):
         # Check if the data in the message is suspicious
-        if not self.msg_data_is_suspicious(msg):
+        if not await self.msg_data_is_suspicious(msg):
             return False
 
         # Load message metadata entities

commit 3aae95b45ea942d51f5506e5321c12e196201748

antibot: Exempt forwarded messages with no sender in content checker
A duplicate of commit 2ae1de52155b5a93b012d958e1940e0ae61b23dc, which is
necessary since we now also call get_sender() in the content checking
wrapper.

Fixes: 94a6b86 ("antibot: Allow forwarding oneself's own messages")
Signed-off-by: Danny Lin <danny@kdrag0n.dev>

pyrobud/modules/antibot.py
@@ -76,12 +76,17 @@ class AntibotModule(module.Module):
         if incoming and has_date:
             # Lazily evalulate suspicious content as it is more expensive
             if forwarded:
+                # Messages forwarded from a linked channel by Telegram don't have a sender
+                # We can assume these messages are safe since only admins can link channels
+                sender = await msg.get_sender()
+                if sender is None:
+                    return False
+
                 # Spambots don't forward their own messages; they mass-forward
                 # messages from central coordinated channels for maximum efficiency
                 # This protects users who forward questions with links/images to
                 # various support chats asking for help (arguably, that's spammy,
                 # but it's not what we're defending against here)
-                sender = await msg.get_sender()
                 if msg.forward.from_id == sender.id or msg.forward.from_name == tg.utils.get_display_name(sender):
                     return False
 