kdrag0n/pyrobud

commit 6c4297bbd0d94a56e72ac861a501dfa7958e04b5

config: Add a unified config upgrading path
To ease future expansion and clean the launcher up.

Signed-off-by: Danny Lin <danny@kdrag0n.dev>

pyrobud/util/config.py
@@ -38,9 +38,6 @@ def upgrade_v2(config, path):
         if os.path.exists("anon.session-journal"):
             os.rename("anon.session-journal", "main.session-journal")
 
-    config["version"] = 2
-    save(config, path)
-
 
 def upgrade_v3(config, path):
     def migrate_antibot(config, db):
@@ -116,9 +113,6 @@ def upgrade_v3(config, path):
         migrate_stats(config, db)
         migrate_stickers(config, db)
 
-    config["version"] = 3
-    save(config, path)
-
 
 def upgrade_v4(config, path):
     bot_config = config["bot"]
@@ -129,9 +123,6 @@ def upgrade_v4(config, path):
         bot_config["report_errors"] = True
         bot_config["report_username"] = False
 
-    config["version"] = 4
-    save(config, path)
-
 
 def upgrade_v5(config, path):
     bot_config = config["bot"]
@@ -139,9 +130,6 @@ def upgrade_v5(config, path):
     if "sentry_dsn" not in bot_config:
         log.info("Adding default Sentry DSN to bot config section")
         bot_config["sentry_dsn"] = ""
-
-    config["version"] = 5
-    save(config, path)
 
 
 def upgrade_v6(config, path):
@@ -151,5 +139,34 @@ def upgrade_v6(config, path):
         log.info("Setting response mode to default 'edit'")
         bot_config["response_mode"] = "edit"
 
-    config["version"] = 6
-    save(config, path)
+
+# Old version -> function to perform migration to new version
+upgrade_funcs = [
+    upgrade_v2,  # 1 -> 2
+    upgrade_v3,  # 2 -> 3
+    upgrade_v4,  # 3 -> 4
+    upgrade_v5,  # 4 -> 5
+    upgrade_v6,  # 5 -> 6
+]
+
+# Master upgrade function
+def upgrade(config, path):
+    # Get current version
+    if "version" in config:
+        cur_version = config["version"]
+    else:
+        cur_version = 1
+
+    # Already at latest version; nothing to do
+    if cur_version == len(upgrade_funcs) - 1:
+        return
+
+    # Upgrade each version sequentially
+    for upgrader in upgrade_funcs[cur_version - 1 :]:
+        target_version = cur_version + 1
+        log.info(f"Upgrading config to version {target_version}")
+        upgrader(config, path)
+        config["version"] = target_version
+
+        # Save config ASAP to prevent an inconsistent state if the next upgrade fails
+        save(config, path)

pyrobud/launch.py
@@ -52,25 +52,7 @@ def main():
         log.info("Initializing Sentry error reporting")
         util.sentry.init()
 
-    if "version" not in config or config["version"] < 2:
-        log.info("Upgrading config to version 2")
-        util.config.upgrade_v2(config, config_path)
-
-    if config["version"] < 3:
-        log.info("Upgrading config to version 3")
-        util.config.upgrade_v3(config, config_path)
-
-    if config["version"] < 4:
-        log.info("Upgrading config to version 4")
-        util.config.upgrade_v4(config, config_path)
-
-    if config["version"] < 5:
-        log.info("Upgrading config to version 5")
-        util.config.upgrade_v5(config, config_path)
-
-    if config["version"] < 6:
-        log.info("Upgrading config to version 6")
-        util.config.upgrade_v6(config, config_path)
+    util.config.upgrade(config, config_path)
 
     log.info("Initializing bot")
     bot = Bot(config)

commit 324234a3467b88f72f4f895b40f5863974e2d5dd

config: Fix logic error in flexible config migrator
This was broken from the beginning, but the breakage was never
encountered because we never bumped the config version by 2 at once
until now. The migrator only worked with a version difference of 1, but
it's surprising that it worked at all with the flaw.

Fixes: 6c4297b ("config: Add a unified config upgrading path")
Signed-off-by: Danny Lin <danny@kdrag0n.dev>

pyrobud/util/config.py
@@ -190,7 +190,7 @@ def upgrade(config: Config, path: str) -> None:
     cur_version: int = config["version"] if "version" in config else 1
 
     # Already at latest version; nothing to do
-    if cur_version == len(upgrade_funcs) - 1:
+    if cur_version == len(upgrade_funcs) + 1:
         return
 
     # Upgrade each version sequentially
@@ -198,6 +198,7 @@ def upgrade(config: Config, path: str) -> None:
         target_version = cur_version + 1
         log.info(f"Upgrading config to version {target_version}")
         upgrader(config, path)
+        cur_version = target_version
         config["version"] = target_version
 
         # Save config ASAP to prevent an inconsistent state if the next upgrade fails