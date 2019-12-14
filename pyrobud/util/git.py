kdrag0n/pyrobud

commit 179a34fa8daa8cfbde0f79be3cbc532ebb4f6f6b

Overhaul with initial type hints and PyCharm inspection fixes
... as well as some other fixes and cleanups here and there.

TODO: fix circular import when mypy runs on module.py and command.py
TODO: deal with missing library implementations/stubs

Signed-off-by: Danny Lin <danny@kdrag0n.dev>

pyrobud/util/git.py
@@ -1,17 +1,19 @@
 import os
 import sys
+from typing import Optional
 
-have_git = True
 try:
     import git
+
+    have_git = True
 except ImportError:
     have_git = False
 
 _repo_initialized = False
-_repo = None
+_repo: Optional[git.Repo] = None
 
 
-def get_repo():
+def get_repo() -> git.Repo:
     global _repo_initialized
     global _repo
 
@@ -36,7 +38,7 @@ def get_repo():
     return _repo
 
 
-def get_current_remote():
+def get_current_remote() -> Optional[git.Remote]:
     repo = get_repo()
     if not repo:
         return None
@@ -48,7 +50,7 @@ def get_current_remote():
     return repo.remote(remote_ref.remote_name)
 
 
-def is_official():
+def is_official() -> bool:
     # Assume non-Git instances are official, e.g. when installed with pip
     repo = get_repo()
     if not repo:

pyrobud/__main__.py
@@ -1,1 +1,3 @@
 from . import launch
+
+launch.main()

pyrobud/bot.py
@@ -4,31 +4,38 @@ import importlib
 import inspect
 import logging
 import os
-import sys
-import traceback
+from typing import Dict, List, Optional, Type, Union, Any
+from types import ModuleType
 
 import aiohttp
 import telethon as tg
-import toml
 import plyvel
 import sentry_sdk
 
-from . import command, module, modules, custom_modules, util
-
-
-class Listener:
-    def __init__(self, event, func, module, priority):
-        self.event = event
-        self.func = func
-        self.module = module
-        self.priority = priority
-
-    def __lt__(self, other):
-        return self.priority < other.priority
+from . import command, module, util
+from .listener import Listener, ListenerFunc
 
 
 class Bot:
-    def __init__(self, config):
+    # Initialized during instantiation
+    commands: Dict[str, command.Command]
+    modules: Dict[str, module.Module]
+    listeners: Dict[str, List[Listener]]
+    config: util.config.Config
+    log: logging.Logger
+    http_session: aiohttp.ClientSession
+    _db: util.db.AsyncDB
+    db: util.db.AsyncDB
+    client: tg.TelegramClient
+
+    # Initialized during startup
+    loop: asyncio.AbstractEventLoop
+    prefix: str
+    user: tg.types.User
+    uid: int
+    start_time_us: int
+
+    def __init__(self, config: util.config.Config):
         # Initialize module dicts
         self.commands = {}
         self.modules = {}
@@ -46,29 +53,29 @@ class Bot:
         self.db = self.get_db("bot")
 
         # Initialize Telegram client
-        tg_config = config["telegram"]
+        tg_config: Dict[str, Union[int, str]] = config["telegram"]
         self.client = tg.TelegramClient(tg_config["session_name"], tg_config["api_id"], tg_config["api_hash"])
 
-    def get_db(self, prefix):
+    def get_db(self, prefix: str) -> util.db.AsyncDB:
         return self._db.prefixed_db(prefix + ".")
 
-    def register_command(self, mod, name, func):
-        info = command.Info(name, mod, func)
+    def register_command(self, mod: module.Module, name: str, func: command.CommandFunc) -> None:
+        cmd = command.Command(name, mod, func)
 
         if name in self.commands:
             orig = self.commands[name]
-            raise module.ExistingCommandError(orig, info)
+            raise module.ExistingCommandError(orig, cmd)
 
-        self.commands[name] = info
+        self.commands[name] = cmd
 
-        for alias in info.aliases:
+        for alias in cmd.aliases:
             if alias in self.commands:
                 orig = self.commands[alias]
-                raise module.ExistingCommandError(orig, info, alias=True)
+                raise module.ExistingCommandError(orig, cmd, alias=True)
 
-            self.commands[alias] = info
+            self.commands[alias] = cmd
 
-    def unregister_command(self, cmd):
+    def unregister_command(self, cmd: command.Command) -> None:
         del self.commands[cmd.name]
 
         for alias in cmd.aliases:
@@ -77,15 +84,18 @@ class Bot:
             except KeyError:
                 continue
 
-    def register_commands(self, mod):
+    def register_commands(self, mod: module.Module) -> None:
         for name, func in util.find_prefixed_funcs(mod, "cmd_"):
+            done = False
+
             try:
                 self.register_command(mod, name, func)
-            except:
-                self.unregister_commands(mod)
-                raise
+                done = True
+            finally:
+                if not done:
+                    self.unregister_commands(mod)
 
-    def unregister_commands(self, mod):
+    def unregister_commands(self, mod: module.Module) -> None:
         # Can't unregister while iterating, so collect commands to unregister afterwards
         to_unreg = []
 
@@ -101,7 +111,7 @@ class Bot:
         for cmd in to_unreg:
             self.unregister_command(cmd)
 
-    def register_listener(self, mod, event, func, priority=100):
+    def register_listener(self, mod: module.Module, event: str, func: ListenerFunc, priority: int = 100) -> None:
         listener = Listener(event, func, mod, priority)
 
         if event in self.listeners:
@@ -109,18 +119,20 @@ class Bot:
         else:
             self.listeners[event] = [listener]
 
-    def unregister_listener(self, listener):
+    def unregister_listener(self, listener: Listener) -> None:
         self.listeners[listener.event].remove(listener)
 
-    def register_listeners(self, mod):
+    def register_listeners(self, mod: module.Module) -> None:
         for event, func in util.find_prefixed_funcs(mod, "on_"):
+            done = True
             try:
-                self.register_listener(mod, event, func, priority=getattr(func, "listener_priority", 100))
-            except:
-                self.unregister_listeners(mod)
-                raise
+                self.register_listener(mod, event, func, priority=getattr(func, "_listener_priority", 100))
+                done = True
+            finally:
+                if not done:
+                    self.unregister_listeners(mod)
 
-    def unregister_listeners(self, mod):
+    def unregister_listeners(self, mod: module.Module) -> None:
         # Can't unregister while iterating, so collect listeners to unregister afterwards
         to_unreg = []
 
@@ -133,7 +145,7 @@ class Bot:
         for listener in to_unreg:
             self.unregister_listener(listener)
 
-    def load_module(self, cls, *, comment=None):
+    def load_module(self, cls: Type[module.Module], *, comment: Optional[str] = None) -> None:
         _comment = comment + " " if comment else ""
         self.log.info(
             f"Loading {_comment}module '{cls.name}' ({cls.__name__}) from '{os.path.relpath(inspect.getfile(cls))}'"
@@ -149,7 +161,7 @@ class Bot:
         self.register_commands(mod)
         self.modules[cls.name] = mod
 
-    def unload_module(self, mod):
+    def unload_module(self, mod: module.Module) -> None:
         _comment = mod.comment + " " if mod.comment else ""
 
         cls = mod.__class__
@@ -161,9 +173,9 @@ class Bot:
         self.unregister_commands(mod)
         del self.modules[cls.name]
 
-    def _load_modules_from_metamod(self, metamod, *, comment=None):
-        for _sym in metamod.__all__:
-            module_mod = getattr(metamod, _sym)
+    def _load_modules_from_metamod(self, metamod: ModuleType, *, comment: str = None) -> None:
+        for _sym in getattr(metamod, "__all__", ()):
+            module_mod: ModuleType = getattr(metamod, _sym)
 
             if inspect.ismodule(module_mod):
                 for sym in dir(module_mod):
@@ -171,13 +183,15 @@ class Bot:
                     if inspect.isclass(cls) and issubclass(cls, module.Module):
                         self.load_module(cls, comment=comment)
 
-    def load_all_modules(self):
+    def load_all_modules(self) -> None:
+        from . import modules, custom_modules
+
         self.log.info("Loading modules")
         self._load_modules_from_metamod(modules)
         self._load_modules_from_metamod(custom_modules, comment="custom")
         self.log.info("All modules loaded.")
 
-    def unload_all_modules(self):
+    def unload_all_modules(self) -> None:
         self.log.info("Unloading modules...")
 
         # Can't modify while iterating, so collect a list first
@@ -186,7 +200,9 @@ class Bot:
 
         self.log.info("All modules unloaded.")
 
-    async def reload_module_pkg(self):
+    async def reload_module_pkg(self) -> None:
+        from . import modules, custom_modules
+
         self.log.info("Reloading base module class...")
         await util.run_sync(lambda: importlib.reload(module))
 
@@ -196,7 +212,7 @@ class Bot:
         self.log.info("Reloading custom master module...")
         await util.run_sync(lambda: importlib.reload(custom_modules))
 
-    def command_predicate(self, event):
+    def command_predicate(self, event: tg.events.NewMessage.Event) -> bool:
         if event.raw_text.startswith(self.prefix):
             parts = event.raw_text.split()
             parts[0] = parts[0][len(self.prefix) :]
@@ -206,7 +222,7 @@ class Bot:
 
         return False
 
-    async def start(self):
+    async def start(self) -> None:
         # Get and store current event loop, since this is the first coroutine
         self.loop = asyncio.get_event_loop()
 
@@ -227,17 +243,19 @@ class Bot:
         # Set Sentry username if enabled
         if self.config["bot"]["report_username"]:
             with sentry_sdk.configure_scope() as scope:
-                scope.user = {"username": self.user.username}
+                scope.set_user({"username": self.user.username})
 
         # Record start time and dispatch start event
         self.start_time_us = util.time.usec()
         await self.dispatch_event("start", self.start_time_us)
 
         # Register handlers
-        self.client.add_event_handler(self.on_message, tg.events.NewMessage)
-        self.client.add_event_handler(self.on_message_edit, tg.events.MessageEdited)
-        self.client.add_event_handler(self.on_command, tg.events.NewMessage(outgoing=True, func=self.command_predicate))
-        self.client.add_event_handler(self.on_chat_action, tg.events.ChatAction)
+        self.client.add_event_handler(self.on_message, tg.events.NewMessage())
+        self.client.add_event_handler(self.on_message_edit, tg.events.MessageEdited())
+        self.client.add_event_handler(
+            self.on_command, tg.events.NewMessage(outgoing=True, func=self.command_predicate),
+        )
+        self.client.add_event_handler(self.on_chat_action, tg.events.ChatAction())
 
         self.log.info("Bot is ready")
 
@@ -246,7 +264,7 @@ class Bot:
         await self.client.catch_up()
         self.log.info("Finished catching up")
 
-    async def stop(self):
+    async def stop(self) -> None:
         await self.dispatch_event("stop")
         await self.http_session.close()
         await self._db.close()
@@ -254,7 +272,7 @@ class Bot:
         self.log.info("Running post-stop hooks")
         await self.dispatch_event("stopped")
 
-    async def dispatch_event(self, event, *args):
+    async def dispatch_event(self, event: str, *args: Any, **kwargs: Any) -> None:
         tasks = set()
 
         try:
@@ -265,26 +283,28 @@ class Bot:
         if not listeners:
             return
 
-        for l in listeners:
-            task = self.loop.create_task(l.func(*args))
+        for lst in listeners:
+            task = self.loop.create_task(lst.func(*args, **kwargs))
             tasks.add(task)
 
         self.log.debug(f"Dispatching event '{event}' with data {args}")
-        return await asyncio.wait(tasks)
+        await asyncio.wait(tasks)
 
-    def dispatch_event_nowait(self, *args, **kwargs):
-        return self.loop.create_task(self.dispatch_event(*args, **kwargs))
+    def dispatch_event_nowait(self, event: str, *args: Any, **kwargs: Any) -> None:
+        self.loop.create_task(self.dispatch_event(event, *args, **kwargs))
 
-    async def on_message(self, event):
+    async def on_message(self, event: tg.events.NewMessage.Event) -> None:
         await self.dispatch_event("message", event)
 
-    async def on_message_edit(self, event):
+    async def on_message_edit(self, event: tg.events.MessageEdited.Event) -> None:
         await self.dispatch_event("message_edit", event)
 
-    async def on_chat_action(self, event):
+    async def on_chat_action(self, event: tg.events.ChatAction.Event) -> None:
         await self.dispatch_event("chat_action", event)
 
-    async def on_command(self, msg):
+    async def on_command(self, msg: tg.events.NewMessage.Event) -> None:
+        cmd = None
+
         try:
             # Attempt to get command info
             try:
@@ -293,7 +313,7 @@ class Bot:
                 return
 
             # Construct invocation context
-            ctx = command.Context(self, msg, msg.segments, len(self.prefix) + len(msg.segments[0]) + 1)
+            ctx = command.Context(self, msg.message, msg.segments, len(self.prefix) + len(msg.segments[0]) + 1,)
 
             # Ensure specified argument needs are met
             if cmd.usage is not None and not cmd.usage_optional and not ctx.input:
@@ -325,14 +345,26 @@ class Bot:
             except Exception as e:
                 cmd.module.log.error(f"Error in command '{cmd.name}'", exc_info=e)
                 await ctx.respond(f"⚠️ Error executing command:\n```{util.format_exception(e)}```")
+
+            await self.dispatch_event("command", cmd, msg)
         except Exception as e:
-            cmd.module.log.error("Error in command handler", exc_info=e)
+            if cmd is not None:
+                cmd.module.log.error("Error in command handler", exc_info=e)
+
             await self.respond(msg, f"⚠️ Error in command handler:\n```{util.format_exception(e)}```")
 
     # Flexible response function with filtering, truncation, redaction, etc.
-    async def respond(self, msg, text=None, *, mode=None, response=None, **kwargs):
+    async def respond(
+        self,
+        msg: tg.types.Message,
+        text: Optional[str] = None,
+        *,
+        mode: Optional[str] = None,
+        response: Optional[tg.types.Message] = None,
+        **kwargs: Any,
+    ) -> tg.types.Message:
         if text is not None:
-            tg_config = self.config["telegram"]
+            tg_config: Dict[str, str] = self.config["telegram"]
             api_id = str(tg_config["api_id"])
             api_hash = tg_config["api_hash"]
 

pyrobud/command.py
@@ -1,52 +1,79 @@
-import logging
+from typing import Any, Callable, List, Optional, TYPE_CHECKING, Coroutine, Union
 
-from . import util
+import telethon as tg
+
+from . import module
+
+if TYPE_CHECKING:
+    from .bot import Bot
+
+CommandFunc = Union[
+    Callable[..., Coroutine[Any, Any, None]], Callable[..., Coroutine[Any, Any, Optional[str]]],
+]
+Decorator = Callable[[CommandFunc], CommandFunc]
 
 
-def desc(_desc):
-    def desc_decorator(func):
-        func.cmd_description = _desc
+def desc(_desc: str) -> Decorator:
+    def desc_decorator(func: CommandFunc) -> CommandFunc:
+        setattr(func, "_cmd_description", _desc)
         return func
 
     return desc_decorator
 
 
-def usage(_usage, optional=False, reply=False):
-    def usage_decorator(func):
-        func.cmd_usage = _usage
-        func.cmd_usage_optional = optional
-        func.cmd_usage_reply = reply
+def usage(_usage: str, optional: bool = False, reply: bool = False) -> Decorator:
+    def usage_decorator(func: CommandFunc) -> CommandFunc:
+        setattr(func, "_cmd_usage", _usage)
+        setattr(func, "_cmd_usage_optional", optional)
+        setattr(func, "_cmd_usage_reply", reply)
         return func
 
     return usage_decorator
 
 
-def alias(*aliases):
-    def alias_decorator(func):
-        if not hasattr(func, "cmd_aliases"):
-            func.cmd_aliases = []
-
-        func.cmd_aliases.extend(aliases)
+def alias(*aliases: str) -> Decorator:
+    def alias_decorator(func: CommandFunc) -> CommandFunc:
+        setattr(func, "_cmd_aliases", aliases)
         return func
 
     return alias_decorator
 
 
-class Info:
-    def __init__(self, name, module, func):
+class Command:
+    name: str
+    desc: str
+    usage: str
+    usage_optional: bool
+    usage_reply: bool
+    aliases: List[str]
+    module: module.Module
+    func: CommandFunc
+
+    def __init__(self, name: str, mod: module.Module, func: CommandFunc) -> None:
         self.name = name
-        self.desc = getattr(func, "cmd_description", None)
-        self.usage = getattr(func, "cmd_usage", None)
-        self.usage_optional = getattr(func, "cmd_usage_optional", False)
-        self.usage_reply = getattr(func, "cmd_usage_reply", False)
-        self.aliases = getattr(func, "cmd_aliases", [])
-        self.module = module
+        self.desc = getattr(func, "_cmd_description", None)
+        self.usage = getattr(func, "_cmd_usage", None)
+        self.usage_optional = getattr(func, "_cmd_usage_optional", False)
+        self.usage_reply = getattr(func, "_cmd_usage_reply", False)
+        self.aliases = getattr(func, "_cmd_aliases", [])
+        self.module = mod
         self.func = func
 
 
 # Command invocation context
 class Context:
-    def __init__(self, bot, msg, segments, cmd_len):
+    bot: "Bot"
+    msg: tg.types.Message
+    segments: List[str]
+    cmd_len: int
+    invoker: str
+
+    response: Optional[tg.types.Message]
+    input: str
+    parsed_input: str
+    args: List[str]
+
+    def __init__(self, bot: "Bot", msg: tg.types.Message, segments: List[str], cmd_len: int) -> None:
         self.bot = bot
         self.msg = msg
         self.segments = segments
@@ -61,18 +88,20 @@ class Context:
         self.parsed_input = self.msg.raw_text[self.cmd_len :]
 
     # Lazily resolve expensive fields
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Any:
         if name == "args":
             return self._get_args()
         else:
             raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
 
     # Argument segments
-    def _get_args(self):
+    def _get_args(self) -> List[str]:
         self.args = self.segments[1:]
         return self.args
 
     # Convenience alias for Bot.respond()
-    async def respond(self, text, *, mode=None, **kwargs):
+    async def respond(
+        self, text: Optional[str] = None, *, mode: Optional[str] = None, **kwargs: Any
+    ) -> tg.types.Message:
         self.response = await self.bot.respond(self.msg, text, mode=mode, response=self.response, **kwargs)
         return self.response

pyrobud/custom_modules/__init__.py
@@ -1,22 +1,30 @@
-import importlib
 import logging
 import os
 import pkgutil
+from importlib import reload as _importlib_reload
+from typing import List
 
-__all__ = list(module for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]))
+log = logging.getLogger("metamod_custom")
+
+
+def reload() -> None:
+    log.info("Reloading custom module classes")
+    for sym in __all__:
+        module = globals()[sym]
+        _importlib_reload(module)
+
+
+__all__: List[str] = list(info.name for info in pkgutil.iter_modules([os.path.dirname(__file__)]))
 
 from . import *
 
 
-log = logging.getLogger("metamod_custom")
-
 try:
-    _reload_flag = _reload_flag
+    _reload_flag: bool
+
+    # noinspection PyUnboundLocalVariable
+    if _reload_flag:
+        # Module has been reloaded, reload our submodules
+        reload()
 except NameError:
     _reload_flag = True
-else:
-    # Module has been reloaded, reload our submodules
-    log.info("Reloading custom module classes")
-    for sym in __all__:
-        module = globals()[sym]
-        importlib.reload(module)

pyrobud/launch.py
@@ -13,7 +13,7 @@ LOG_FORMAT = "  %(log_color)s%(levelname)-8s%(reset)s | %(name)-7s | %(log_color
 log = logging.getLogger("launch")
 
 
-def setup_logging():
+def setup_logging() -> None:
     logging.root.setLevel(LOG_LEVEL)
     formatter = colorlog.ColoredFormatter(LOG_FORMAT)
 
@@ -26,7 +26,7 @@ def setup_logging():
     root.addHandler(stream)
 
 
-def setup_loop():
+def setup_loop() -> None:
     # While uvloop is in our requirements.txt, it's not required by any means
     # and doesn't work in Termux due to their patched libuv
     try:
@@ -34,17 +34,17 @@ def setup_loop():
 
         uvloop.install()
     except ImportError:
-        log.warn("Unable to load uvloop; falling back to default asyncio event loop")
+        log.warning("Unable to load uvloop; falling back to default asyncio event loop")
 
 
-def main():
+def main() -> None:
     config_path = "config.toml"
 
     setup_logging()
     setup_loop()
 
     log.info("Loading config")
-    config = toml.load(config_path)
+    config: util.config.Config = toml.load(config_path)
 
     # Initialize Sentry reporting here to exempt config syntax errors and query
     # the user's report_errors value, defaulting to enabled if not specified
@@ -63,7 +63,7 @@ def main():
     try:
         loop.run_until_complete(bot.start())
     except KeyboardInterrupt:
-        log.warn("Received interrupt while connecting; exiting")
+        log.warning("Received interrupt while connecting; exiting")
         return
 
     bot.client.run_until_disconnected()

pyrobud/listener.py
@@ -1,6 +1,29 @@
-def priority(_prio):
-    def prio_decorator(func):
-        func.listener_priority = _prio
+from typing import Callable, Any
+
+
+ListenerFunc = Any
+Decorator = Callable[[ListenerFunc], ListenerFunc]
+
+
+def priority(_prio: int) -> Decorator:
+    def prio_decorator(func: ListenerFunc) -> ListenerFunc:
+        setattr(func, "_listener_priority", _prio)
         return func
 
     return prio_decorator
+
+
+class Listener:
+    event: str
+    func: ListenerFunc
+    module: Any
+    priority: int
+
+    def __init__(self, event: str, func: ListenerFunc, mod: Any, prio: int) -> None:
+        self.event = event
+        self.func = func
+        self.module = mod
+        self.priority = prio
+
+    def __lt__(self, other: "Listener") -> bool:
+        return self.priority < other.priority

pyrobud/module.py
@@ -1,4 +1,21 @@
 import logging
+from typing import Type, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .bot import Bot
+    from .command import Command
+
+
+class Module:
+    name = "Unnamed"
+    bot: "Bot"
+    log: logging.Logger
+    comment: Optional[str]
+
+    def __init__(self, bot: "Bot") -> None:
+        self.bot = bot
+        self.log = logging.getLogger(self.__class__.name.lower().replace(" ", "_"))
+        self.comment = None
 
 
 class ModuleLoadError(Exception):
@@ -6,7 +23,10 @@ class ModuleLoadError(Exception):
 
 
 class ExistingModuleError(ModuleLoadError):
-    def __init__(self, old_module, new_module):
+    old_module: Type[Module]
+    new_module: Type[Module]
+
+    def __init__(self, old_module: Type[Module], new_module: Type[Module]) -> None:
         super().__init__(f"Module '{old_module.name}' ({old_module.__name__}) already exists")
 
         self.old_module = old_module
@@ -14,20 +34,18 @@ class ExistingModuleError(ModuleLoadError):
 
 
 class ExistingCommandError(ModuleLoadError):
-    def __init__(self, old_cmd, new_cmd, alias=False):
+    old_cmd: "Command"
+    new_cmd: "Command"
+    alias: bool
+
+    def __init__(self, old_cmd: "Command", new_cmd: "Command", alias: bool = False) -> None:
         al_str = "alias of " if alias else ""
+        old_name = old_cmd.module.__class__.__name__
+        new_name = new_cmd.module.__class__.__name__
         super().__init__(
-            f"Attempted to replace existing command '{old_cmd.name}' (from {old_cmd.module.__class__.__name__}) with {al_str}'{new_cmd.name}' (from {new_cmd.module.__class__.__name__})"
+            f"Attempt to replace existing command '{old_cmd.name}' (from {old_name}) with {al_str}'{new_cmd.name}' (from {new_name})"
         )
 
         self.old_cmd = old_cmd
         self.new_cmd = new_cmd
         self.alias = alias
-
-
-class Module:
-    name = "Unnamed"
-
-    def __init__(self, bot):
-        self.bot = bot
-        self.log = logging.getLogger(self.__class__.name.lower().replace(" ", "_"))

commit 2904537bb86e5f93ca01fc1a99a82410a5d7381c

git: Quote all GitPython references in type hints
We support running without GitPython since it's not essential at all
(only used for checking official status for Sentry reporting and the
convenient update command). This is useful for reducing the size of our
Docker image since it's completely unnecessary in that context
(updates are administered by updating the Docker image and users can't
tamper with the code).

Adding type hints breaks startup when the module doesn't load properly
(i.e. when Git isn't installed), so work around the issue by quoting the
type hints so type checkers lazily evaluate them as necessary.

Fixes: 179a34f ("Overhaul with initial type hints and PyCharm inspection fixes")
Signed-off-by: Danny Lin <danny@kdrag0n.dev>

pyrobud/util/git.py
@@ -10,10 +10,10 @@ except ImportError:
     have_git = False
 
 _repo_initialized = False
-_repo: Optional[git.Repo] = None
+_repo: "Optional[git.Repo]" = None
 
 
-def get_repo() -> git.Repo:
+def get_repo() -> "git.Repo":
     global _repo_initialized
     global _repo
 
@@ -38,7 +38,7 @@ def get_repo() -> git.Repo:
     return _repo
 
 
-def get_current_remote() -> Optional[git.Remote]:
+def get_current_remote() -> "Optional[git.Remote]":
     repo = get_repo()
     if not repo:
         return None