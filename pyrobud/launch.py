kdrag0n/pyrobud

commit e1ffc630a4c549c82292b802221e955e19bdb567

launch: Use aiorun to start and manage the event loop
This ensures that everything is stopped and cleaned up properly, which
is surprisingly hard when asyncio is in use. It also handles Ctrl-C
better, especially on Windows, which should be helpful for debugging.

Signed-off-by: Danny Lin <danny@kdrag0n.dev>

pyrobud/launch.py
@@ -1,25 +1,28 @@
-import asyncio
 import logging
 
+import aiorun
 import tomlkit
 
 from . import DEFAULT_CONFIG_PATH, util
 from .core import Bot
 
 log = logging.getLogger("launch")
+# Suppress most messages from aiorun's overly verbose logger
+aiorun.logger.setLevel(logging.WARNING)
 
 
-def setup_asyncio(config: util.config.Config) -> None:
+def get_use_uvloop(config: util.config.Config) -> bool:
     asyncio_config: util.config.AsyncIOConfig = config["asyncio"]
 
     # Initialize uvloop if enabled, available, and working
     if asyncio_config["use_uvloop"]:
         try:
             import uvloop
-
-            uvloop.install()
         except ImportError:
             log.warning("Unable to load uvloop; falling back to default asyncio event loop")
+            return False
+        else:
+            return True
 
 
 def main(*, config_path: str = DEFAULT_CONFIG_PATH) -> None:
@@ -36,9 +39,8 @@ def main(*, config_path: str = DEFAULT_CONFIG_PATH) -> None:
 
     util.config.upgrade(config, config_path)
 
-    setup_asyncio(config)
+    use_uvloop = get_use_uvloop(config)
     log.info("Initializing bot")
     bot = Bot(config)
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(bot.run())
+    aiorun.run(bot.run(), use_uvloop=use_uvloop)

pyproject.toml
@@ -54,6 +54,7 @@ pillow = "^6.2.1"
 speedtest-cli = "^2.1.2"
 # Debug module
 meval = "^2.5"
+aiorun = "^2019.11.1"
 
 [tool.poetry.extras]
 fast = ["uvloop"]

pyrobud/core/telegram_bot.py
@@ -18,6 +18,7 @@ class TelegramBot(MixinBase):
     tg_config: TelegramConfig
 
     # Initialized during startup
+    client: tg.TelegramClient
     loop: asyncio.AbstractEventLoop
     prefix: str
     user: tg.types.User
@@ -25,6 +26,10 @@ class TelegramBot(MixinBase):
     start_time_us: int
 
     def __init__(self: "Bot", **kwargs: Any) -> None:
+        # Propagate initialization to other mixins
+        super().__init__(**kwargs)
+
+    async def init_client(self: "Bot") -> None:
         # Get Telegram parameters from config and check types
         self.tg_config = self.config["telegram"]
 
@@ -43,11 +48,9 @@ class TelegramBot(MixinBase):
         # Initialize Telegram client with gathered parameters
         self.client = tg.TelegramClient(session_name, api_id, api_hash)
 
-        # Propagate initialization to other mixins
-        super().__init__(**kwargs)
-
     async def start(self: "Bot") -> None:
         self.log.info("Starting")
+        await self.init_client()
 
         # Get and store current event loop, since this is the first coroutine that runs
         self.loop = asyncio.get_event_loop()

commit 4b42b413f91933a9a70e645a1d7e39c30029af8a

launch: Fix missing return if uvloop is disabled
Fixes: e1ffc63 ("launch: Use aiorun to start and manage the event loop")
Signed-off-by: Danny Lin <danny@kdrag0n.dev>

pyrobud/launch.py
@@ -18,11 +18,13 @@ def get_use_uvloop(config: util.config.Config) -> bool:
     if asyncio_config["use_uvloop"]:
         try:
             import uvloop
+
+            return True
         except ImportError:
             log.warning("Unable to load uvloop; falling back to default asyncio event loop")
             return False
-        else:
-            return True
+    else:
+        return False
 
 
 def main(*, config_path: str = DEFAULT_CONFIG_PATH) -> None: