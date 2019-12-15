kdrag0n/pyrobud

commit bc08f8fa94632156e1f51a100437cb3bd9b7caa9

Revamp command invocation system and add centralized argument validation
This reduces the dependency on introspection and other bad practices,
such as injecting functions and variables into Telethon's Message class.

Other minor edits are also included, such as the removal of the Hastebin
command due to how unreliable it has been recently.

Signed-off-by: Danny Lin <danny@kdrag0n.dev>

pyrobud/command.py
@@ -1,5 +1,7 @@
 import logging
 
+from . import util
+
 
 def desc(_desc):
     def desc_decorator(func):
@@ -7,6 +9,16 @@ def desc(_desc):
         return func
 
     return desc_decorator
+
+
+def usage(_usage, optional=False, reply=False):
+    def usage_decorator(func):
+        func.cmd_usage = _usage
+        func.cmd_usage_optional = optional
+        func.cmd_usage_reply = reply
+        return func
+
+    return usage_decorator
 
 
 def alias(*aliases):
@@ -24,6 +36,42 @@ class Info:
     def __init__(self, name, module, func):
         self.name = name
         self.desc = getattr(func, "cmd_description", None)
+        self.usage = getattr(func, "cmd_usage", None)
+        self.usage_optional = getattr(func, "cmd_usage_optional", False)
+        self.usage_reply = getattr(func, "cmd_usage_reply", False)
         self.aliases = getattr(func, "cmd_aliases", [])
         self.module = module
         self.func = func
+
+
+# Command invocation context
+class Context:
+    def __init__(self, bot, msg, segments, cmd_len):
+        self.bot = bot
+        self.msg = msg
+        self.segments = segments
+        self.cmd_len = cmd_len
+        self.invoker = segments[0]
+
+        # Response message to be filled later
+        self.response = None
+        # Single argument string (unparsed, i.e. complete with Markdown formatting symbols)
+        self.input = self.msg.text[self.cmd_len :]
+        # Single argument string (parsed, i.e. plain text)
+        self.parsed_input = self.msg.raw_text[self.cmd_len :]
+
+    # Lazily resolve expensive fields
+    def __getattr__(self, name):
+        if name == "args":
+            return self._get_args()
+        else:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+
+    # Argument segments
+    def _get_args(self):
+        self.args = self.segments[1:]
+        return self.args
+
+    # Convenience alias for Bot.respond()
+    async def respond(self, text, *, mode=None, **kwargs):
+        return await self.bot.respond(self.msg, text, mode=mode, response=self.response, **kwargs)

pyrobud/bot.py
@@ -229,47 +229,6 @@ class Bot:
             with sentry_sdk.configure_scope() as scope:
                 scope.user = {"username": self.user.username}
 
-        # Hijack Message class to provide result function
-        async def result(msg, new_text, mode=None, **kwargs):
-            tg_config = self.config["telegram"]
-            api_id = str(tg_config["api_id"])
-            api_hash = tg_config["api_hash"]
-
-            # Redact sensitive information
-            if api_id in new_text:
-                new_text = new_text.replace(api_id, "[REDACTED]")
-            if api_hash in new_text:
-                new_text = new_text.replace(api_hash, "[REDACTED]")
-            if self.user.phone in new_text:
-                new_text = new_text.replace(self.user.phone, "[REDACTED]")
-
-            # Default to disabling link previews in responses
-            if "link_preview" not in kwargs:
-                kwargs["link_preview"] = False
-
-            # Truncate messages longer than Telegram's 4096-character length limit
-            truncated_suffix = "... (truncated)"
-            if len(new_text) > 4096:
-                new_text = new_text[: 4096 - len(truncated_suffix)] + truncated_suffix
-
-            # Use selected response mode if not overridden by invoker
-            if mode is None:
-                mode = self.config["bot"]["response_mode"]
-
-            if mode == "edit":
-                await msg.edit(text=new_text, **kwargs)
-            elif mode == "reply":
-                if hasattr(msg, "_result_msg"):
-                    # Already replied, so just edit the existing reply to reduce spam
-                    await msg._result_msg.edit(text=new_text, **kwargs)
-                else:
-                    # Reply since we haven't done so yet
-                    msg._result_msg = await msg.reply(new_text, **kwargs)
-            else:
-                raise ValueError(f"Unknown response mode '{mode}'")
-
-        tg.types.Message.result = result
-
         # Record start time and dispatch start event
         self.start_time_us = util.time.usec()
         await self.dispatch_event("start", self.start_time_us)
@@ -325,52 +284,84 @@ class Bot:
     async def on_chat_action(self, event):
         await self.dispatch_event("chat_action", event)
 
-    async def on_command(self, event):
+    async def on_command(self, msg):
         try:
+            # Attempt to get command info
             try:
-                cmd_info = self.commands[event.segments[0]]
+                cmd = self.commands[msg.segments[0]]
             except KeyError:
                 return
 
-            cmd_func = cmd_info.func
-            cmd_spec = inspect.getfullargspec(cmd_func)
-            cmd_args = cmd_spec.args
+            # Construct invocation context
+            ctx = command.Context(self, msg, msg.segments, len(self.prefix) + len(msg.segments[0]) + 1)
 
-            args = []
-            if len(cmd_args) == 3:
-                txt = event.text
+            # Ensure specified argument needs are met
+            if cmd.usage is not None and not cmd.usage_optional and not ctx.input:
+                err_base = f"⚠️ Missing parameters: {cmd.usage}"
 
-                # Contrary to typical terms, text = raw text (i.e. with Markdown formatting)
-                # and raw_text = parsed text (i.e. plain text without formatting symbols)
-                if cmd_args[2].startswith("parsed_"):
-                    txt = event.raw_text
+                if cmd.usage_reply:
+                    if msg.is_reply:
+                        reply_msg = await msg.get_reply_message()
+                        if reply_msg.text:
+                            ctx.input = reply_msg.text
+                            ctx.parsed_input = reply_msg.raw_text
+                        else:
+                            await ctx.respond(f"{err_base}\n__The message you replied to doesn't contain text.__")
+                            return
+                    else:
+                        await ctx.respond(f"{err_base} (replying is also supported)")
+                        return
+                else:
+                    await ctx.respond(err_base)
+                    return
 
-                args = [txt[len(self.prefix) + len(event.segments[0]) + 1 :]]
-            elif cmd_spec.varargs and not cmd_spec.kwonlyargs:
-                args = event.segments[1:]
-
+            # Invoke command function
             try:
-                ret = await cmd_func(event, *args)
+                ret = await cmd.func(ctx)
+
+                # Response shortcut
+                if ret is not None:
+                    await ctx.respond(ret)
             except Exception as e:
-                cmd_info.module.log.error(f"Error in command '{cmd_info.name}'", exc_info=e)
-                ret = f"⚠️ Error executing command:\n```{util.format_exception(e)}```"
-
-            if ret is not None:
-                try:
-                    await event.result(ret)
-                except Exception as e:
-                    cmd_info.module.log.error(
-                        "Error updating message with data returned by command '%s'", cmd_info.name, exc_info=e,
-                    )
-                    ret = f"⚠️ Error updating message:\n```{util.format_exception(e)}```"
-
-                    await event.result(ret)
-
-            await self.dispatch_event("command", event, cmd_info, args)
+                cmd.module.log.error(f"Error in command '{cmd.name}'", exc_info=e)
+                await ctx.respond(f"⚠️ Error executing command:\n```{util.format_exception(e)}```")
         except Exception as e:
-            try:
-                await event.result(f"⚠️ Error in command handler:\n```{util.format_exception(e)}```")
-            except Exception:
-                raise
+            cmd.module.log.error("Error in command handler", exc_info=e)
+            await self.respond(msg, f"⚠️ Error in command handler:\n```{util.format_exception(e)}```")
 
-            raise
+    # Flexible response function with filtering, truncation, redaction, etc.
+    async def respond(self, msg, text, *, mode=None, response=None, **kwargs):
+        tg_config = self.config["telegram"]
+        api_id = str(tg_config["api_id"])
+        api_hash = tg_config["api_hash"]
+
+        # Redact sensitive information
+        if api_id in text:
+            text = text.replace(api_id, "[REDACTED]")
+        if api_hash in text:
+            text = text.replace(api_hash, "[REDACTED]")
+        if self.user.phone in text:
+            text = text.replace(self.user.phone, "[REDACTED]")
+
+        # Default to disabling link previews in responses
+        if "link_preview" not in kwargs:
+            kwargs["link_preview"] = False
+
+        # Truncate messages longer than Telegram's 4096-character length limit
+        text = util.tg.truncate(text)
+
+        # Use selected response mode if not overridden by invoker
+        if mode is None:
+            mode = self.config["bot"]["response_mode"]
+
+        if mode == "edit":
+            return await msg.edit(text=text, **kwargs)
+        elif mode == "reply":
+            if response is not None:
+                # Already replied, so just edit the existing reply to reduce spam
+                return await response.edit(text=text, **kwargs)
+            else:
+                # Reply since we haven't done so yet
+                return await msg.reply(text, **kwargs)
+        else:
+            raise ValueError(f"Unknown response mode '{mode}'")

pyrobud/modules/antibot.py
@@ -239,18 +239,18 @@ class AntibotModule(module.Module):
             await self.take_action(action, user)
 
     @command.desc("Toggle the antibot auto-moderation feature in this group")
-    async def cmd_antibot(self, msg):
-        if not msg.is_group:
+    async def cmd_antibot(self, ctx: command.Context):
+        if not ctx.msg.is_group:
             return "__Antibot can only be used in groups.__"
 
-        if not msg.is_channel:
+        if not ctx.msg.is_channel:
             return "__Please convert this group to a supergroup in order to enable antibot.__"
 
-        state = not await self.group_db.get(f"{msg.chat_id}.enabled", False)
+        state = not await self.group_db.get(f"{ctx.msg.chat_id}.enabled", False)
 
         if state:
             # Check for required permissions
-            chat = await msg.get_chat()
+            chat = await ctx.msg.get_chat()
             ch_participant = await self.bot.client(tg.tl.functions.channels.GetParticipantRequest(chat, "me"))
             ptcp = ch_participant.participant
 
@@ -264,10 +264,10 @@ class AntibotModule(module.Module):
             else:
                 return "__I must be an admin with the **Delete Messages** and **Ban users** permissions for antibot to work.__"
 
-            await self.group_db.put(f"{msg.chat_id}.enabled", True)
-            await self.group_db.put(f"{msg.chat_id}.enable_time", util.time.sec())
+            await self.group_db.put(f"{ctx.msg.chat_id}.enabled", True)
+            await self.group_db.put(f"{ctx.msg.chat_id}.enable_time", util.time.sec())
         else:
-            await self.clear_group(msg.chat_id)
+            await self.clear_group(ctx.msg.chat_id)
 
         status = "enabled" if state else "disabled"
         return f"Antibot is now **{status}** in this group."

pyrobud/modules/bot_setup.py
@@ -117,29 +117,36 @@ Please read the rules _before_ participating.
         return new_list
 
     @command.desc("Set up @MissRose_bot and derivatives")
-    async def cmd_bsetup(self, msg, input_cfg):
-        if not msg.is_group:
+    @command.usage("[config?]", optional=True)
+    async def cmd_bsetup(self, ctx: command.Context):
+        input_cfg = ctx.input
+
+        if not ctx.msg.is_group:
             return "__This feature can only be used in groups.__"
 
-        parse_results = self.parse_config(msg.chat_id, input_cfg)
+        parse_results = self.parse_config(ctx.msg.chat_id, input_cfg)
         if isinstance(parse_results, str):
             # A string return value means an error occurred, so propagate it
             return parse_results
 
         target, rule_str, button_str, parsed_cfg = parse_results
-        commands = self.get_commands(msg.chat_id, rule_str, button_str)
+        commands = self.get_commands(ctx.msg.chat_id, rule_str, button_str)
         formatted_cfg = toml.dumps(parsed_cfg)
+        if formatted_cfg:
+            settings_used = f"\n```{formatted_cfg}```"
+        else:
+            settings_used = " defaults\n"
 
         before = datetime.now()
 
         status_header = f"Setting up @{target} via PM connection..."
-        await msg.result(status_header)
+        await ctx.respond(status_header)
 
         try:
-            await self.promote_bot(msg.chat, target)
+            await self.promote_bot(ctx.msg.chat, target)
         except Exception as e:
             status_header += f"\n**WARNING**: Unable to promote @{target}: `{str(e)}`"
-            await msg.result(status_header)
+            await ctx.respond(status_header)
 
         async with self.bot.client.conversation(target) as conv:
 
@@ -159,10 +166,11 @@ Please read the rules _before_ participating.
 Commands issued:
 ```{cmd_log}```"""
 
-                await msg.result(status)
+                await ctx.respond(status)
 
                 # Wait for both the rate-limit and the bot's response
                 try:
+                    # pylint: disable=unused-variable
                     done, pending = await asyncio.wait((reply_and_ack(), asyncio.sleep(0.25)))
 
                     # Raise all exceptions
@@ -176,8 +184,7 @@ Commands issued:
 
                     return f"""Setup of @{target} failed after {delta_seconds} seconds.
 
-Settings used:
-```{formatted_cfg}```
+Settings used:{settings_used}
 Commands issued:
 ```{cmd_log}```
 
@@ -188,7 +195,6 @@ The bot failed to respond within 1 minute of issuing the last command. Perhaps t
 
         return f"""Setup of @{target} finished in {delta_seconds} seconds.
 
-Settings used:
-```{formatted_cfg}```
+Settings used:{settings_used}
 Commands issued:
 ```{cmd_log}```"""

pyrobud/modules/core.py
@@ -1,5 +1,3 @@
-import inspect
-
 from .. import command, module, util
 
 OFFICIAL_SUPPORT_LINK = "https://t.me/pyrobud"
@@ -9,7 +7,10 @@ class CoreModule(module.Module):
     name = "Core"
 
     @command.desc("List the commands")
-    async def cmd_help(self, msg, filt):
+    @command.usage("[filter: command or module name?]", optional=True)
+    async def cmd_help(self, ctx: command.Context):
+        filt = ctx.input
+
         lines = {}
 
         # Handle command filters
@@ -20,24 +21,23 @@ class CoreModule(module.Module):
                 # Generate aliases section
                 aliases = f"`{'`, `'.join(cmd.aliases)}`" if cmd.aliases else "none"
 
-                # Generate arguments section
-                cmd_func = cmd.func
-                cmd_spec = inspect.getfullargspec(cmd_func)
-                cmd_args = cmd_spec.args
-
-                if len(cmd_args) == 3:
-                    args_desc = "Yes, one string"
-                elif cmd_spec.varargs and not cmd_spec.kwonlyargs:
-                    args_desc = "Yes, whitespace-separated segments"
+                # Generate parameters section
+                if cmd.usage is None:
+                    args_desc = "none"
                 else:
-                    args_desc = "No"
+                    args_desc = cmd.usage
+
+                    if cmd.usage_optional:
+                        args_desc += " (optional)"
+                    if cmd.usage_reply:
+                        args_desc += " (also accepts replies)"
 
                 # Show info card
                 return f"""`{cmd.name}`: **{cmd.desc if cmd.desc else '__No description provided.__'}**
 
 Module: {cmd.module.name}
 Aliases: {aliases}
-Takes arguments: {args_desc}"""
+Expected parameters: {args_desc}"""
             else:
                 return "__That filter didn't match any commands or modules.__"
 
@@ -71,12 +71,15 @@ Takes arguments: {args_desc}"""
         return "\n".join(sections)
 
     @command.desc("Get how long the bot has been up for")
-    async def cmd_uptime(self, msg):
+    async def cmd_uptime(self, ctx: command.Context):
         delta_us = util.time.usec() - self.bot.start_time_us
         return f"Uptime: {util.time.format_duration_us(delta_us)}"
 
     @command.desc("Get or change the bot prefix")
-    async def cmd_prefix(self, msg, new_prefix):
+    @command.usage("[new prefix?]", optional=True)
+    async def cmd_prefix(self, ctx: command.Context):
+        new_prefix = ctx.input
+
         if not new_prefix:
             return f"The prefix is `{self.bot.prefix}`."
 
@@ -86,5 +89,5 @@ Takes arguments: {args_desc}"""
         return f"Prefix set to `{self.bot.prefix}`."
 
     @command.desc("Get the link to the official bot support group")
-    async def cmd_support(self, msg):
+    async def cmd_support(self, ctx: command.Context):
         return f"[Join the official bot support group for help.]({OFFICIAL_SUPPORT_LINK})"

pyrobud/modules/debug.py
@@ -13,19 +13,18 @@ class DebugModule(module.Module):
     name = "Debug"
 
     @command.desc("Evaluate code")
+    @command.usage("[code snippet]")
     @command.alias("ev", "exec")
-    async def cmd_eval(self, msg, raw_args):
-        code = util.tg.filter_code_block(raw_args)
-        if not code:
-            return "__I need code to execute.__"
+    async def cmd_eval(self, ctx: command.Context):
+        code = util.tg.filter_code_block(ctx.input)
 
         async def _eval(code):
-            # Send helper for convenience
-            async def send(text):
-                return await msg.respond(text)
+            # Message sending helper for convenience
+            async def send(*args, **kwargs):
+                return await ctx.msg.respond(*args, **kwargs)
 
             try:
-                return ("", await meval(code, globals(), send=send, self=self, msg=msg, raw_args=raw_args))
+                return ("", await meval(code, globals(), send=send, self=self, ctx=ctx))
             except Exception as e:
                 # Find first traceback frame involving the snippet
                 first_snip_idx = -1
@@ -61,9 +60,10 @@ class DebugModule(module.Module):
 Time: {el_str}"""
 
     @command.desc("Get the code of a command")
-    async def cmd_src(self, msg, cmd_name):
-        if cmd_name is None or len(cmd_name) < 1:
-            return "__Command name required to get source code.__"
+    @command.usage("[command name]")
+    async def cmd_src(self, ctx: command.Context):
+        cmd_name = ctx.input
+
         if cmd_name not in self.bot.commands:
             return f"__Command__ `{cmd_name}` __doesn't exist.__"
 
@@ -73,58 +73,38 @@ Time: {el_str}"""
 
     @command.desc("Get plain text of a message")
     @command.alias("text", "raw")
-    async def cmd_gtx(self, msg):
-        if not msg.is_reply:
+    async def cmd_gtx(self, ctx: command.Context):
+        if not ctx.msg.is_reply:
             return "__Reply to a message to get the text of.__"
 
-        reply_msg = await msg.get_reply_message()
-        await msg.result(reply_msg.text, parse_mode=None)
+        reply_msg = await ctx.msg.get_reply_message()
+        await ctx.respond(reply_msg.text, parse_mode=None)
 
     @command.desc("Send text")
-    async def cmd_echo(self, msg, text):
-        if not text:
-            return "__Provide text to send.__"
-
+    @command.usage("[text to send]")
+    async def cmd_echo(self, ctx: command.Context):
+        text = ctx.input
         return text
 
     @command.desc("Dump all the data of a message")
     @command.alias("md", "msginfo", "minfo")
-    async def cmd_mdump(self, msg):
-        if not msg.is_reply:
+    async def cmd_mdump(self, ctx: command.Context):
+        if not ctx.msg.is_reply:
             return "__Reply to a message to get its data.__"
 
-        reply_msg = await msg.get_reply_message()
+        reply_msg = await ctx.msg.get_reply_message()
         data = reply_msg.stringify()
 
         return f"```{data}```"
 
-    @command.desc("Send media by file ID")
-    @command.alias("file")
-    async def cmd_fileid(self, msg, file_id):
-        if not file_id and not msg.is_reply:
-            return "__Provide a file ID to send or reply to a message with media to get its ID.__"
-
-        if file_id:
-            reply_msg = await msg.get_reply_message() if msg.is_reply else None
-
-            await msg.result("Sending media...")
-            await msg.respond(reply_to=reply_msg, file=file_id)
-            await msg.delete()
-        else:
-            rep = await msg.get_reply_message()
-            if not rep.media:
-                return "__Provide a file ID to send or reply to a message with media to get its ID.__"
-
-            if msg.file:
-                return f"File ID: `{msg.file.id}`"
-
-            return "__No compatible media found.__"
-
-    @command.desc("Get all available information about the given entity (or `chat`)")
+    @command.desc("Get all available information about the given entity")
+    @command.usage('[entity ID/username/... or "chat" for the current chat?, or reply]', optional=True)
     @command.alias("einfo")
-    async def cmd_entity(self, msg, entity_str):
+    async def cmd_entity(self, ctx: command.Context):
+        entity_str = ctx.input
+
         if entity_str == "chat":
-            entity = await msg.get_chat()
+            entity = await ctx.msg.get_chat()
         elif entity_str:
             if entity_str.isdigit():
                 try:
@@ -136,8 +116,8 @@ Time: {el_str}"""
                 entity = await self.bot.client.get_entity(entity_str)
             except ValueError as e:
                 return f"Error getting entity `{entity_str}`: {e}"
-        elif msg.is_reply:
-            entity = await msg.get_reply_message()
+        elif ctx.msg.is_reply:
+            entity = await ctx.msg.get_reply_message()
         else:
             return "__No entity given via argument or reply.__"
 
@@ -145,16 +125,16 @@ Time: {el_str}"""
 
     @command.desc("Get all contextually relevant IDs")
     @command.alias("user", "info")
-    async def cmd_id(self, msg):
+    async def cmd_id(self, ctx: command.Context):
         lines = []
 
-        if msg.chat_id:
-            lines.append(f"Chat ID: `{msg.chat_id}`")
+        if ctx.msg.chat_id:
+            lines.append(f"Chat ID: `{ctx.msg.chat_id}`")
 
         lines.append(f"My user ID: `{self.bot.uid}`")
 
-        if msg.is_reply:
-            reply_msg = await msg.get_reply_message()
+        if ctx.msg.is_reply:
+            reply_msg = await ctx.msg.get_reply_message()
             sender = await reply_msg.get_sender()
             lines.append(f"Message ID: `{reply_msg.id}`")
 

pyrobud/modules/manager.py
@@ -6,21 +6,21 @@ class ManagerModule(module.Module):
 
     @command.desc("Reload all modules")
     @command.alias("ra", "reload", "r")
-    async def cmd_reloadall(self, msg):
+    async def cmd_reloadall(self, ctx: command.Context):
         before = util.time.usec()
 
         await self.bot.dispatch_event("stop")
 
-        await msg.result("Unloading all modules...")
+        await ctx.respond("Unloading all modules...")
         self.bot.unload_all_modules()
 
-        await msg.result("Reloading module classes...")
+        await ctx.respond("Reloading module classes...")
         await self.bot.reload_module_pkg()
 
-        await msg.result("Loading new modules...")
+        await ctx.respond("Loading new modules...")
         self.bot.load_all_modules()
 
-        await msg.result("Dispatching events...")
+        await ctx.respond("Dispatching events...")
         await self.bot.dispatch_event("load")
         await self.bot.dispatch_event("start", util.time.usec())
 

commit 0bbc9e1b5b667f07f1ce167fe4e493184f90a132

context: respond: Discard old response when switching modes
Some commands, such as `s` and `sp`, will call ctx.respond() with the
default mode ("edit" in most cases) to provide an "Uploading" status
update, then call it again with the "repost" mode to send the actual
response -- usually to send a different type of message than the
invoking one.

This fixes the following error:
MessageNotModifiedError: Content of the message was not modified (caused by EditMessageRequest)

Fixes: bc08f8f ("Revamp command invocation system and add centralized argument validation")
Signed-off-by: Danny Lin <danny@kdrag0n.dev>

pyrobud/command.py
@@ -67,6 +67,7 @@ class Context:
     invoker: str
 
     response: Optional[tg.custom.Message]
+    response_mode: Optional[str]
     input: str
     parsed_input: str
     args: Sequence[str]
@@ -80,6 +81,7 @@ class Context:
 
         # Response message to be filled later
         self.response = None
+        self.response_mode = None
         # Single argument string (unparsed, i.e. complete with Markdown formatting symbols)
         self.input = self.msg.text[self.cmd_len :]
         # Single argument string (parsed, i.e. plain text)
@@ -102,6 +104,12 @@ class Context:
         self, text: Optional[str] = None, *, mode: Optional[str] = None, redact: Optional[bool] = None, **kwargs: Any
     ) -> tg.custom.Message:
         self.response = await self.bot.respond(
-            self.msg, text, mode=mode, redact=redact, response=self.response, **kwargs
+            self.msg,
+            text,
+            mode=mode,
+            redact=redact,
+            response=self.response if mode == self.response_mode else None,
+            **kwargs,
         )
+        self.response_mode = mode
         return self.response