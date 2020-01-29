f2794e80f4c9b97084cf5b220c54f74ef92284eb
Danny Lin <danny@kdrag0n.dev>
2020-01-28 21:54:51 -0800

    time: Simplify formatting of longer durations
    Nobody cares about seconds once you're describing a duration in the
    range of hours or days.

--- pyrobud/util/time.py
+++ pyrobud/util/time.py
@@ -25,13 +25,10 @@ def format_duration_us(t_us: Union[int, float]) -> str:
 
     if t_d >= 1:
         rem_h = t_h % 24
-        rem_m = t_m % 60
-        rem_s = t_s % (24 * 60 * 60) % 60
-        return "%dd %dh %dm %ds" % (t_d, rem_h, rem_m, rem_s)
+        return "%dd %dh" % (t_d, rem_h)
     elif t_h >= 1:
         rem_m = t_m % 60
-        rem_s = t_s % (60 * 60) % 60
-        return "%dh %dm %ds" % (t_h, rem_m, rem_s)
+        return "%dh %dm" % (t_h, rem_m)
     elif t_m >= 1:
         rem_s = t_s % 60
         return "%dm %ds" % (t_m, rem_s)
