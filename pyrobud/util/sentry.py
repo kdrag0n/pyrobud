4db52158fe5ad64f015772740967043177831139
Danny Lin <danny@kdrag0n.dev>
2020-04-10 00:43:36 -0700

    sentry: Fix checks and make filter more resilient

--- pyrobud/util/sentry.py
+++ pyrobud/util/sentry.py
@@ -8,12 +8,14 @@ import ratelimit
 import sentry_sdk
 
 from .. import __version__
-from . import git, version
+from . import error, git, version
 
 Event = Dict[str, Any]
 EventHint = Dict[str, Any]
 
-PUBLIC_CLIENT_KEY = "https://75fe67fda0594284b2c3aea6b90a1ba7@sentry.io/1817585"
+PUBLIC_CLIENT_KEY = (
+    "https://75fe67fda0594284b2c3aea6b90a1ba7@o120089.ingest.sentry.io/1817585"
+)
 
 log = logging.getLogger("sentry")
 
@@ -25,47 +27,56 @@ def _ratelimit() -> None:
 
 
 def _send_filter(event: Event, hint: EventHint) -> Optional[Event]:
-    # Discard event if ratelimit is exceeded
     try:
-        _ratelimit()
-    except ratelimit.RateLimitException:
-        return None
-
-    if "exc_info" in hint:
-        exc_type: Type[BaseException]
-        exc_value: BaseException
-        exc_type, exc_value, = hint["exc_info"]
-
-        # User-initiated interrupts, network errors, and I/O errors
-        if exc_type in (
-            KeyboardInterrupt,
-            ConnectionError,
-            IOError,
-            sqlite3.OperationalError,
-        ):
+        # Discard event if ratelimit is exceeded
+        try:
+            _ratelimit()
+        except ratelimit.RateLimitException:
             return None
 
-        exc_msg = str(exc_value)
+        if "exc_info" in hint:
+            exc_type: Type[BaseException]
+            exc_value: BaseException
+            exc_type, exc_value = hint["exc_info"][:2]
 
-        # Pillow error for invalid user-submitted images
-        if exc_msg.startswith("cannot identify image file"):
-            return None
+            # User-initiated interrupts, network errors, and I/O errors
+            if exc_type in (
+                KeyboardInterrupt,
+                ConnectionError,
+                IOError,
+                sqlite3.OperationalError,
+            ):
+                return None
 
-        # Telegram connection errors
-        if exc_msg.startswith("Automatic reconnection failed") or exc_msg.startswith(
-            "Request was unsuccessful"
-        ):
-            return None
+            exc_msg = str(exc_value)
 
-        # Check involved files
-        if getattr(exc_value, "__traceback__", None):
-            tb = traceback.extract_tb(exc_value.__traceback__)
-            for frame in tb:
-                # Ignore custom module errors
-                if Path(frame.filename).parent == "custom_modules":
-                    return None
+            # Pillow error for invalid user-submitted images
+            if exc_msg.startswith("cannot identify image file"):
+                return None
 
-    return event
+            # Telegram connection errors
+            if exc_msg.startswith(
+                "Automatic reconnection failed"
+            ) or exc_msg.startswith("Request was unsuccessful"):
+                return None
+
+            # Check involved files
+            if getattr(exc_value, "__traceback__", None):
+                tb = traceback.extract_tb(exc_value.__traceback__)
+                for frame in tb:
+                    # Ignore custom module errors
+                    if Path(frame.filename).parent.stem == "custom_modules":
+                        return None
+
+        return event
+    except Exception as e:
+        log.error(f"Error running event filter", exc_info=e)
+
+        # Inject error and return
+        if "extra" not in event:
+            event["extra"] = {}
+        event["extra"]["send_filter_error"] = error.format_exception(e)
+        return event
 
 
 def init() -> None:
