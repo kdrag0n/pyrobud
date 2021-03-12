57e0f8e6400c9a49bde0c99310044ed28816fc9a
Danny Lin <danny@kdrag0n.dev>
2021-03-12 14:26:51 -0800

    network: Remove Dogbin integration
    A replacement will be added later, but Dogbin is being removed due to
    recent events involving its owner.

--- pyrobud/modules/network.py
+++ pyrobud/modules/network.py
@@ -33,31 +33,6 @@ class NetworkModule(module.Module):
         if match is not None:
             self.log.info(f"Received Telegram login code: {match.group(1)}")
 
-    @command.desc("Paste message text to Dogbin")
-    @command.alias("deldog", "dogbin")
-    @command.usage(
-        "[text to paste?, or upload/reply to message or file]", optional=True
-    )
-    async def cmd_dog(self, ctx: command.Context) -> str:
-        input_text = ctx.input
-
-        status, text = await util.tg.get_text_input(ctx, input_text)
-        if not status:
-            if isinstance(text, str):
-                return text
-
-            return "__Unknown error.__"
-
-        await ctx.respond("Uploading text to [Dogbin](https://del.dog/)...")
-
-        async with self.bot.http.post("https://del.dog/documents", data=text) as resp:
-            try:
-                resp_data = await resp.json()
-            except aiohttp.ContentTypeError:
-                return "__Dogbin is currently experiencing issues. Try again later.__"
-
-            return f'https://del.dog/{resp_data["key"]}'
-
     @command.desc("Upload given file to file.io")
     @command.usage("[expiry time?]", optional=True)
     async def cmd_fileio(self, ctx: command.Context) -> str:
